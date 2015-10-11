ec7bffaa80b01eea2e73d0a4b976f7db1bde344a
Sebastian Osthoff <sebastian.osthoff@hrs.de>
2015-10-11 02:02:24 +0200

    Initial Commit

---
+++ minis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  minis
+//
+//  Created by Sebastian Osthoff on 11/10/15.
+//  Copyright © 2015 Sebastian Osthoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
